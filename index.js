9f6ac14ae7a35b885251a58eb902ce3979eb9800
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-10 06:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const app = "I don't do much."
 kittens= ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(name){
-  
+  kittens.push(name)
 }
 function destructivelyPrependKitten(name){
-  
+  kittens.unshift(name)
 }
 function function destructivelyRemoveLastKitten(){
-  
+  kittens.pop(name)
 }
 function destructivelyRemoveFirstKitten(){
-  
+  kittens.shift()
 }
 function appendKitten(name){
-  
+  //return Kittens.splice(0).push(name)
 }
 function prependKitten(name){
-  
+  //return Kittens.splice(0,)
 }
 function removeLastKitten(){
-  
+  return Kittens.slice(0,Kittens.length-2)
 }
 function removeFirstKitten(){
-  
+  return Kittens.splice(1)
 }
